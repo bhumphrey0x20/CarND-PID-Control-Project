550857316d63dd020a8780c8a943e568a4c9197e
bhumphrey0x20 <38211805+bhumphrey0x20@users.noreply.github.com>
2018-10-24 14:26:04 -0500

    Update PID.cpp

--- src/PID.cpp
+++ src/PID.cpp
@@ -1,21 +1,54 @@
+#include <iostream>
 #include "PID.h"
+
 
 using namespace std;
 
-/*
-* TODO: Complete the PID class.
-*/
 
-PID::PID() {}
+
+
+
+PID::PID() {
+//init: Kp,   Ki,    Kd	
+	//Init(0.03, 0.03, 1.5); 
+	//Init(0.03, 0.03, 1.6);	
+Init(0.03, 0.01, 0.7);
+}
 
 PID::~PID() {}
 
-void PID::Init(double Kp, double Ki, double Kd) {
+void PID::Init(double Kp_, double Ki_, double Kd_) {
+	Kp = Kp_;
+	Ki = Ki_;
+	Kd = Kd_;
+
 }
 
 void PID::UpdateError(double cte) {
+	static double prev_error = 0.0;
+	
+	// update proportional error term
+	p_error = cte; 
+
+	// update integral error term
+	i_error += cte;
+
+	// update derivative error term
+	d_error = cte - prev_error; 	
+
+	prev_error = cte;
 }
 
 double PID::TotalError() {
+	return p_error + i_error + d_error;
 }
+
+
+double PID::pid_cntl(){
+
+	double steer = - Kp * p_error - Ki * i_error - Kd * d_error;
+	return steer; 
+}
+
+
 
