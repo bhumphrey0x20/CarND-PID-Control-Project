4d294ca401014703a768d71d5cd552fa011bc4e1
bhumphrey0x20 <38211805+bhumphrey0x20@users.noreply.github.com>
2018-10-25 08:27:13 -0500

    edit

--- src/main.cpp
+++ src/main.cpp
@@ -59,7 +59,7 @@ int main()
           */
           
           // DEBUG
-          /std::cout << "CTE: " << cte << " Steering Value: " << steer_value << std::endl;
+          //std::cout << "CTE: " << cte << " Steering Value: " << steer_value << std::endl;
 
           json msgJson;
           msgJson["steering_angle"] = steer_value;
