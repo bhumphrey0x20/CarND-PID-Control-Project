0f7022415b10121709904d137df103c57e314050
bhumphrey0x20 <38211805+bhumphrey0x20@users.noreply.github.com>
2018-10-31 12:39:08 -0500

    added speed filter for pid calc

--- src/PID.cpp
+++ src/PID.cpp
@@ -10,7 +10,9 @@ using namespace std;
 
 PID::PID() {
 	//init: Kp,   Ki,    Kd	
-	Init(0.03, 0.01, 1.5);
+	//Init(0.03, 0.01, 1.5);
+//	Init(0.15, 0.02, 4.2);
+	Init(0.15, 0.0, 4.2);
 }
 
 PID::~PID() {}
@@ -42,11 +44,33 @@ double PID::TotalError() {
 }
 
 
-double PID::pid_cntl(){
+double PID::pid_cntl(double speed){
+	static const int max_arr = 3;
+	static double speed_array[3] = {0.0,0.0,0.0}; 
+	static int index = 0; 
+	double speed_avg = 0.0;
 
+//filtering speed values
+	speed_array[index++] = speed; 
+	if( index >max_arr) index = 0;
+
+	for (int i = 0; i < max_arr; i++){
+		speed_avg += speed_array[i];
+	}
+	speed_avg = speed_avg/max_arr;	
+//set gains based of avg speed threshold
+	if (speed_avg < 25.0){
+		Kp = 0.15;
+		Ki = 0.0;
+		Kd = 4.2;
+	}else{
+
+		Kp = 0.25;
+		Ki = 0.0225;
+		Kd = 4.15;
+	}
 	double steer = - Kp * p_error - Ki * i_error - Kd * d_error;
 	return steer; 
 }
-
 
 
